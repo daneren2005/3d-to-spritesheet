45846ff47312677361cb6c41358594af6061d95b
Scott Jackson <daneren2005@gmail.com>
2022-03-27 16:19:53 -0700

    Start of adding a deploy to pages script

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['warn', 'error'] }] : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-empty': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-case-declarations': 'off',
 		'semi': 'error',
